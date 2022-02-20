62bb99a73e12fd3ba61ac5b39291e40f6ca7661e
546pvp <79662987+546pvp@users.noreply.github.com>
2022-02-20 14:32:50 +0100

    Create rickroll2.user.js

---
+++ rickroll2.user.js
@@ -0,0 +1,6 @@
+// ==/UserScript==
+// @name rickroll-script
+// @author 546pvp
+// @include http://*
+// @include https://*
+location.href = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiwm8-7sI72AhVGO-wKHSBgDJUQwqsBegQIBhAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DxvFZjo5PgG0&usg=AOvVaw1bR_FN-SnfIGtKD3fjAA5P"
